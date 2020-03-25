3dc0136a5b59eb7c4960cd8a1164a221b7cdb25c
Marlo <marlo.baguyos@outlook.com>
2020-03-24 22:36:20 -0400

    update

--- Lab1/ERP/src/app/app-routing.module.ts
+++ Lab1/ERP/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { ConverterComponent } from './components/converter/converter.component';
 
 //register the routes (paths) for your pages
 const routes: Routes = [
     {path: '', component: MainPageComponent},
-    {path: 'contact', component: ContactPageComponent}
+    {path: 'contact', component: ContactPageComponent},
+    {path: 'converter', component: ConverterComponent}
 ];
 
 @NgModule({
