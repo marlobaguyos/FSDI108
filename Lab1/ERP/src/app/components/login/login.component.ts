8edc726e90f4681b65d80ff6ad818fd18203d44d
Marlo <marlo.baguyos@outlook.com>
2020-03-31 20:45:11 -0400

    update

--- Lab1/ERP/src/app/components/login/login.component.ts
+++ Lab1/ERP/src/app/components/login/login.component.ts
@@ -5,11 +5,27 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  userName = '';
+  password = '';
+  allUsers = [];
 
-  ngOnInit(): void {
+  constructor(private data: DataService) {
+    //get all users
+    this.allUsers = data.userList;
+  }
+
+  login() {
+    //travel the list of users
+    //compare each user credentials with values from the form
+
+    for (let i = 0; i < this.allUsers.length; i++){
+      var user = this.allUsers[i];
+      if(user.userName == this.userName && user.password == this.password){
+        console.log("Logged in correctly!");
+      }
+    }
   }
 
 }
