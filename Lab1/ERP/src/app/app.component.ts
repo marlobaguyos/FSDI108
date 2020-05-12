7ad095bd28a0182122460a22648162371aa2b394
Marlo <marlo.baguyos@outlook.com>
2020-05-11 21:59:53 -0400

    update

--- Lab1/ERP/src/app/app.component.ts
+++ Lab1/ERP/src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ERP';
+  myName = 'Marlo Baguyos'
+
+  constructor(){
+    console.log("hello, I'm the constructor")
+
+    var name: string = "Marlo Baguyos"
+    var age: number = 99;
+    var found: boolean = false;
+    var list: string[] = [];
+
+    this.hello(this.title);
+
+  }
+
+  hello(name){
+    console.log(name);
+  }
+
+  something(){
+    console.log("Btn clicked");
+    this.title="Whatever you want";
+  }
+
+
 }
